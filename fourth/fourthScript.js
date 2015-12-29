36932a43f582affee3a093f61a478b1487447e66
Donutttt <orr2@live.com>
2015-12-29 13:49:59 +0000

    added select options, remove

--- fourth/fourthScript.js
+++ fourth/fourthScript.js
@@ -8,8 +8,26 @@ function FourthViewModel(){
   self.inputName = ko.observable();
   self.inputValue = ko.observable();
 
+  self.selectChoice = ko.observable();
+
+  self.menuChoices = ko.observableArray(
+    [
+      {id: 1, title: "First"},
+      {id: 2, title: "Another One"},
+      {id: 3, title: "Another One"}
+    ]
+  );
+
   self.addNew = function(){
     self.pointlessHolder.addValue(new ValuePair(self.inputName(), self.inputValue()));
+  };
+
+  self.updateSelect = function(){
+    console.log("Select changed to id: " + self.selectChoice());
+  };
+
+  self.removePair = function(pair){
+    self.pointlessHolder.values.remove(pair);
   };
 }
 
@@ -21,7 +39,6 @@ var Holder = function(){
 Holder.prototype.addValue = function(valueToAdd){
   var self = this;
   self.values.push(valueToAdd);
-
   console.log(self.values().length);
 };
 
